55dedbca468fcbef8de9ed07f8a42fa21145f4d8
jagruti123 <jagrutinand19@gmail.com>
2023-11-30 08:02:53 +0530

    commit changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,20 @@ import './App.css'
 import Login from "./components/Login"
 import Register from "./components/Register";
 import Home from "./components/Home"
+import PropertyDetails from "./pages/PropertyDetails";
+import BasicInfo from "./pages/BasicInfo";
+import GeneralInfo from "./pages/GeneralInfo";
+import Locationinfo from "./pages/Locationinfo";
+import SideBar from "./components/SideBar";
+import NewPage from "./pages/NewPage";
+import Assistance from "./pages/Assistance";
+import SentInterest from "./pages/SentInterest";
+import Property from "./pages/Property";
+import PropertyViews from "./pages/PropertyViews";
+import TariffPlan from "./pages/TariffPlan";
+import RecievedInterest from "./pages/RecievedInterest";
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import AppRoutes from "./components/AppRoutes";
 
 function App() {
